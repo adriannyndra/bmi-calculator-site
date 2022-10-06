1f3982f6fb01a4dfeade0ea5db1d168cc67415eb
Aprilliano <114375139+adriannyndra@users.noreply.github.com>
2022-10-06 15:49:48 +0700

    Improved the conclusion script to display health range better

--- javascript.js
+++ javascript.js
@@ -4,10 +4,24 @@ function calculations () {
     var bmi = w/(h/100*h/100);
     var bmio = (bmi.toFixed(2));
 
-    if (bmio < 18.5 || bmio > 24.9) {
+    if (bmio < 18.5) {
+        document.getElementById("result").style.color = "Blue";
+        document.getElementById("conclusion").innerHTML="You are underweight";
+    } else if (bmio > 24.9 && bmio < 30){
+        document.getElementById("result").style.color = "Yellow"
+        document.getElementById("conclusion").innerHTML="You are overweight";
+
+    } else if (bmio > 30 && bmio < 35){
+        document.getElementById("result").style.color = "Orange"
+        document.getElementById("conclusion").innerHTML="You are obese";
+
+    } else if (bmio > 35){
         document.getElementById("result").style.color = "Red"
+        document.getElementById("conclusion").innerHTML="You are EXTREMELY obese";
+
     } else {
         document.getElementById("result").style.color = "Green"
+        document.getElementById("conclusion").innerHTML="Your BMI is normal";
     }
 
     document.getElementById("result").innerHTML="Your BMI is " + bmio
