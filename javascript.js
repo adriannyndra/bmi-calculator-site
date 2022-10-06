c76d276eaae50ffb785abacd6235ebfa6b781ba4
Aprilliano <114375139+adriannyndra@users.noreply.github.com>
2022-10-06 15:16:15 +0700

    Added script to change result color based on health range

--- javascript.js
+++ javascript.js
@@ -4,5 +4,11 @@ function calculations () {
     var bmi = w/(h/100*h/100);
     var bmio = (bmi.toFixed(2));
 
+    if (bmio < 18.5 || bmio > 24.9) {
+        document.getElementById("result").style.color = "Red"
+    } else {
+        document.getElementById("result").style.color = "Green"
+    }
+
     document.getElementById("result").innerHTML="Your BMI is " + bmio
 }
